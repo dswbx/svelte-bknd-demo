b2f3be178f377f27d4c804e1513643f7fde34e47
dswbx <dennis.senn@gmx.ch>
2025-11-23 21:36:53 +0100

    add bknd admin

--- src/bknd.ts
+++ src/bknd.ts
@@ -1,8 +1,20 @@
-import { createRuntimeApp } from "bknd/adapter";
+import { createRuntimeApp, serveStaticViaImport } from "bknd/adapter";
 import config from "../bknd.config";
 
+export async function getApp() {
+   return await createRuntimeApp(
+      {
+         ...config,
+         adminOptions: {
+            adminBasepath: "/admin",
+         },
+      },
+      import.meta.env
+   );
+}
+
 export async function getApi() {
-   const app = await createRuntimeApp(config, import.meta.env);
+   const app = await getApp();
    const api = app.getApi();
    return api;
 }
